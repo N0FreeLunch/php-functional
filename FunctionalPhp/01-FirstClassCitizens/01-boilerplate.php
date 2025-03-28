e3ad5b135f2c8e553ed318166018a9668ff7af3c
N0FreeLunch <consilience90@gmail.com>
2025-03-28 23:34:38 +0900

    add sample code

--- FunctionalPhp/01-FirstClassCitizens/01-boilerplate.php
+++ FunctionalPhp/01-FirstClassCitizens/01-boilerplate.php
@@ -1,40 +1,55 @@
 <?php
-  function getPrices1(array $products) {
-    $prices = [];
-    foreach($products as $p) {
-      if($p->stock > 0) {
-        $prices[] = $p->price;
-      }
-    }
-    return $price;
-  }
 
-  function getPrices2(array $products) {
-    $prices = [];
-    foreach($products as $p) {
-      if($p -> stock > 0) {
-        $prices[] = $p->price;
-      }
-    }
-    return $prices;
-  }
-
-  function my_function ($parameter, $second_parameter) {
-
-  }
-
-
-  class SomeClass {
-    private $some_property;
-
-    public function some_function ()
-    {
-
-    }
-
-    static protected function other_function ()
-    {
-
+function getPrices1(array $products) {
+  $prices = [];
+  foreach($products as $p) {
+    if($p->stock > 0) {
+      $prices[] = $p->price;
     }
   }
- ?>
+  return $prices;
+}
+
+function getPrices2(array $products) {
+  $prices = [];
+  foreach($products as $p) {
+    if($p->stock > 0) {
+      $prices[] = $p->price;
+    }
+  }
+  return $prices;
+}
+
+class Product
+{
+  public readonly int $price;
+  public readonly int $stock;
+
+  public function setRandomPrice(): self
+  {
+    $this->price = mt_rand(1000, 9999);
+    return $this;
+  }
+
+  public function setRandomStock(): self
+  {
+    $this->stock = mt_rand(0, 100);
+    return $this;
+  }
+}
+
+$products = [
+  (new Product)->setRandomPrice()->setRandomStock(),
+  (new Product)->setRandomPrice()->setRandomStock(),
+  (new Product)->setRandomPrice()->setRandomStock(),
+  (new Product)->setRandomPrice()->setRandomStock(),
+  (new Product)->setRandomPrice()->setRandomStock(),
+];
+
+$result1 = getPrices1($products);
+
+var_dump($result1);
+
+$result2 = getPrices2($products);
+
+var_dump($result2);
