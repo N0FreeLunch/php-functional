b5e8b553be7b7b7982050401ee1bb2db549428a7
N0FreeLunch <consilience90@gmail.com>
2025-03-31 21:33:42 +0900

    update strict mode code

--- FunctionalPhp/01-FirstClassCitizens/03-strict_mode.php
+++ FunctionalPhp/01-FirstClassCitizens/03-strict_mode.php
@@ -1,61 +1,77 @@
-<?php
+<?php declare(strict_types=1);
 
-declare(strict_types=1);
+echo "use strict mode \n";
 
-$n = '<br>';
+echo "strict mode check function parameter type and return type strictly \n";
 
 function add(float $a, int $b): float
 {
   return $a + $b;
 }
 
-echo "add".$n;
+printf("result of add(3.5, 1) is %.2f \n", add(3.5, 1));
 
-echo add(3.5, 1).$n;
+printf("result of add(3, 1) is %.2f \n", add(3, 1));
 
-echo add(3, 1).$n;
+try {
+  printf("result of add(3.5, 1.2) is %.2f \n", add(3.5, 1.2));
+} catch (Error $e) {
+  printf("error of add(3.5, 1.2) : %s \n", $e->getMessage());
+}
 
-// error
-// echo add("3.5", 1).$n;
+try {
+  printf("result of add('3.5', 1) is %.2f \n", add('3.5', 1));
+} catch (Error $e) {
+  printf("error of add('3.5', 1) : %s \n", $e->getMessage());
+}
 
-// error
-// echo add(3.5, 1.2).$n;
+try {
+  printf("result of add('1 weel', 1) is %.2f \n", add('1 weel', 1));
+} catch (Error $e) {
+  printf("error of add('1 weel', 1) : %s \n", $e->getMessage());
+}
 
-
-// error
-// echo add("1 weel", 1).$n;
-
-//error
-// echo add("some string", 1).$n;
-
-
+try {
+  printf("result of add('some string', 1) is %.2f \n", add('some string', 1));
+} catch (Error $e) {
+  printf("error of add('some string', 1) : %s \n", $e->getMessage());
+}
 
 function test_bool(bool $a): string
 {
   return $a ? 'true' : 'false';
 }
 
-echo "test_bool".$n;
+printf("result of test_bool(true) is %s \n", test_bool(true));
 
-echo test_bool(true).$n;
+printf("result of test_bool(false) is %s \n", test_bool(false));
 
-echo test_bool(false).$n;
+try {
+  printf("result of test_bool('some string') is %s \n", test_bool("some string"));
+} catch (Error $e) {
+  printf("error of test_bool('some string') : %s \n", $e->getMessage());
+}
 
-// error
-// echo test_bool("").$n;
+try {
+  printf("result of test_bool('') is %s \n", test_bool(""));
+} catch (Error $e) {
+  printf("error of test_bool('') : %s \n", $e->getMessage());
+}
 
-// error
-// echo test_bool("some string").$n;
+try {
+  printf("result of test_bool(0) is %s \n", test_bool(0));
+} catch (Error $e) {
+  printf("error of test_bool(0) : %s \n", $e->getMessage());
+}
 
-// error
-// echo test_bool(0).$n;
+try {
+  printf("result of test_bool(1) is %s \n", test_bool(1));
+} catch (Error $e) {
+  printf("error of test_bool(1) : %s \n", $e->getMessage());
+}
 
-// error
-// echo test_bool(1).$n;
-
-// error
-// echo test_bool([]).$n;
-
-
-
- ?>
+try {
+  printf("result of test_bool([]) is %s \n", test_bool([]));
+} catch (Error $e) {
+  printf("error of test_bool([]) : %s \n", $e->getMessage());
+}
