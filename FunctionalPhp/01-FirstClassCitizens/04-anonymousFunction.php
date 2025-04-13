f2460050a019ebb0fefcba1989dd3f1bf1e32c5d
N0FreeLunch <consilience90@gmail.com>
2025-04-13 23:46:54 +0900

    update anonymousFunction

--- FunctionalPhp/01-FirstClassCitizens/04-anonymousFunction.php
+++ FunctionalPhp/01-FirstClassCitizens/04-anonymousFunction.php
@@ -1,30 +1,27 @@
-<?php
+<?php declare(strict_types=1);
 
 $add = function(float $a, float $b): float {
   return $a + $b;
 };
 
 $result = $add(5, 10);
-echo $result;
-echo '<br>';
+var_dump($result);
 
 $sum = array_reduce([1, 2, 3, 4, 5], $add, 0);
 var_dump($sum);
-echo '<br>';
 
 $uppercase = array_map(function(string $s): string {
   return strtoupper($s);
 },['hello', 'world']);
 
 var_dump($uppercase);
-echo '<br>';
 
-function return_new_function ()
+function returnNewFunction()
 {
   return function($a, $b, $c) {
     return [$a, $b, $c];
   };
 }
-var_dump(return_new_function()(1,2,3));
 
- ?>
+printf("named function: %s\n", returnNewFunction()::class); // named function: Closure
+var_dump(returnNewFunction()(1,2,3));
