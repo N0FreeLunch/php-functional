65c299baa05528a0087c5a6bf728f917de41df14
N0FreeLunch <consilience90@gmail.com>
2025-04-15 02:08:41 +0900

    define closure as static

--- FunctionalPhp/01-FirstClassCitizens/07-ErrorClassClosure.php
+++ FunctionalPhp/01-FirstClassCitizens/07-ErrorClassClosure.php
@@ -1,19 +1,42 @@
-<?php
+<?php declare(strict_types=1);
 
 class ClosureInsideClass
 {
-  public function testing()
+  const FOO = 'foo';
+
+  public function returnThisCapturedStaticClosure(): Closure
   {
     return (static function() {
-      // static cause error
       var_dump($this);
+    });
+  }
+
+  public function returnThisCapturedClosure(): Closure
+  {
+    return (function() {
+      var_dump($this);
+    });
+  }
+
+  public function returnStaticClosureWithStaticMember(): Closure
+  {
+    return (static function() {
+      var_dump(self::FOO);
     });
   }
 };
 
 $object = new ClosureInsideClass();
-$test = $object -> testing();
 
-$test();
+try {
+  $closure = $object->returnThisCapturedStaticClosure();
+  $closure();
+} catch (Throwable $e) {
+  printf("returnThisCapturedStaticClosure : %s ".PHP_EOL, $e->getMessage());
+}
 
- ?>
+$closure = $object->returnThisCapturedClosure();
+$closure();
+
+$closure = $object->returnStaticClosureWithStaticMember();
+$closure();
