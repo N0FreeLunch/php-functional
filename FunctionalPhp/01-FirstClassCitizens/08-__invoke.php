0c94e23edbbae0670b21d6468bbbf955fafe1566
N0FreeLunch <consilience90@gmail.com>
2025-04-15 23:36:01 +0900

    update __invoke, call class instance

--- FunctionalPhp/01-FirstClassCitizens/08-__invoke.php
+++ FunctionalPhp/01-FirstClassCitizens/08-__invoke.php
@@ -1,27 +1,29 @@
-<?php
+<?php declare(strict_types=1);
 
 class ObjectAsFunction
 {
-  private function helper(int $a, int $b) : int
+  public function __invoke(int $a, int $b): int
+  {
+    return $this->add($a, $b);
+  }
+
+  public function callAdd(int $a, int $b): int
+  {
+    return $this->add($a, $b);
+  }
+
+  private function add(int $a, int $b): int
   {
     return $a + $b;
   }
-
-  public function __invoke(int $a, int $b): int
-  {
-    return $this->helper($a, $b);
-  }
-
-  // error
-  // public function callHelper(int $a, int $b) : int
-  // {
-  //   return helper($a, $b);
-  // }
-
 }
 
 $instance = new ObjectAsFunction();
-echo $instance(5, 10);
-// echo $instance->callHelper(5, 10);
+printf("call instance \$instance(5, 10): %d\n", $instance(5, 10));
+printf("call callAdd method of \$instance: %d\n", $instance->callAdd(5, 10));
 
- ?>
+try {
+  printf("call private add method of \$instance: %d\n", $instance->add(5, 10));
+} catch (Throwable $e) {
+  printf("error to call private add method of \$instance: %s\n", $e->getMessage());
+}
