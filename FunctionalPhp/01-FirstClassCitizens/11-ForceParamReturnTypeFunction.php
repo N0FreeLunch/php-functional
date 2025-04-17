8f6caee3a032851f28e16ce06763b76747bcbfaf
N0FreeLunch <consilience90@gmail.com>
2025-04-17 23:57:45 +0900

    update ForceParamReturnTypeFunction.php

--- FunctionalPhp/01-FirstClassCitizens/11-ForceParamReturnTypeFunction.php
+++ FunctionalPhp/01-FirstClassCitizens/11-ForceParamReturnTypeFunction.php
@@ -1,46 +1,10 @@
-<?php
+<?php declare(strict_types=1);
 
-function test_callable(callable $callback) : callable {
-  $callback();
-  return function () {
-
-  };
-}
-var_dump(test_callable(function () {
-  return "callable";
-}));
-
-echo "<br>";
-
-var_dump(test_callable(function () {
-  return "callable";
-})());
-
-echo "<br>";
-
-function return_function(callable $callback) : callable {
-  // $callback();
+function callableParamFn(callable $callback) : callable {
   return $callback;
 }
 
-var_dump(return_function(function () {
-  return "callable";
-})());
-
-
-// function error_callable(callable $callback) :
-// callable {
-//   $callback();
-//   return "error";
-// }
-//
-// var_dump(error_callable());
-
+printf("call returned callable by passed callable".PHP_EOL, callableParamFn(function () { return "callable"; })());
 
 $callback = 'strtoupper';
-echo "<br>";
-echo $callback;
-echo "<br>";
-echo $callback('Hello World!');
-
- ?>
+printf('call built-in function by string callable: %s'.PHP_EOL, $callback('hello world'));
