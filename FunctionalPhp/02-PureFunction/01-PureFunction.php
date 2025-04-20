100745a28d7050fcf67b05f99d8e69895b25b862
N0FreeLunch <consilience90@gmail.com>
2025-04-20 11:16:39 +0900

    update pure function

--- FunctionalPhp/02-PureFunction/01-PureFunction.php
+++ FunctionalPhp/02-PureFunction/01-PureFunction.php
@@ -1,14 +1,39 @@
-<?php
+<?php declare(strict_types=1);
+
 function add(int $a, int $b):int
 {
   return $a + $b;
 }
 
+printf("The result of pure function add(5, 6): %s".PHP_EOL, add(5, 6));
+
 function updateScore(Player $player, int $points)
 {
-  $score = $player -> getScore();
+  $score = $player->getScore();
   $player->setScore($score + $points);
 }
 
-var_dump(add(5, 6));
- ?>
+$player = new Player(5);
+updateScore($player, 6);
+
+printf("The result of non-pure function \$player->getScore() after \$player = new Player(5); updateScore(\$player, 6): %s".PHP_EOL, $player->getScore());
+
+class Player
+{
+  private int $score = 0;
+
+  public function __construct(int $initScore = 0)
+  {
+    $this->score = $initScore;
+  }
+
+  public function getScore(): int
+  {
+    return $this->score;
+  }
+
+  public function setScore(int $score): void
+  {
+    $this->score = $score;
+  }
+}
